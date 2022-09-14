400c693c5cb86391acfd130f87cbc399556d65f3
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2022-09-14 12:40:58 +0100

    Rush V2
    Separated the movement and RUSH scripts

--- Endless_Runner/Assets/Scripts/PlayerController.cs
+++ Endless_Runner/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public int runspeed = 1;
-    public int sprintspeed = 1;
     public int playerspeed = 1;
     public float jumpforce = 1;
-    public int rushtime = 200;
-    public int rush = 1;
     bool isOnGround = false;
 
     public GameObject groundChecker;
@@ -27,23 +23,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame 
     void Update()
     {
-         
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            rush = rushtime;
-        }
-
-        if (rush > 1)
-        {
-            playerspeed = sprintspeed;
-            rush = rush - 1;
-        }
-
-        if (rush == 1)
-        {
-            playerspeed = runspeed;
-        }
-
         float movementValueX = Input.GetAxis("Horizontal");
         playerObject.velocity = new Vector2 (movementValueX * playerspeed, playerObject.velocity.y);
 
