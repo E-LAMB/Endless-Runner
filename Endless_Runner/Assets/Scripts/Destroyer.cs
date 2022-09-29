951830f04f9459b984265b1ce1e70572c7341494
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2022-09-29 12:46:17 +0100

    FogWall

--- Endless_Runner/Assets/Scripts/Destroyer.cs
+++ Endless_Runner/Assets/Scripts/Destroyer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Destroyer : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public string target = "n/a";
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collison)
     {
         
-        if (collison.gameObject.tag == "Clean")
+        if (collison.gameObject.tag == target)
         {
             Destroy(collison.gameObject);
         }
