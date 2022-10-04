a222135e2ca67635ef92dab2703b1709e8be2fa6
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2022-10-04 10:48:27 +0100

    Ball

--- Endless_Runner/Assets/Scripts/BarrierMoving.cs
+++ Endless_Runner/Assets/Scripts/BarrierMoving.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class BarrierMoving : MonoBehaviour
 {
 
-    public float speed = 1.0f;
+    public float startspeed = 7.0f;
+    public float maxspeed = 10f;
+    public float incrementspeed = 0.1f;
+
+    public int increment_progress = 0;
+    public int increment_requirement = 1000;
+    public int increment_increase = 500; 
 
     Rigidbody2D playerObject; 
 
@@ -18,6 +24,17 @@ public class BarrierMoving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerObject.velocity = new Vector2 (speed,0);
+        increment_progress = increment_progress + 1;
+        if (increment_progress > increment_requirement)
+        {
+            increment_progress = 0;
+            increment_requirement = increment_requirement + increment_increase;
+
+            if (startspeed < maxspeed)
+            {
+                startspeed = startspeed + incrementspeed;
+            }
+        }
+        playerObject.velocity = new Vector2 (startspeed,0);
     }
 }
